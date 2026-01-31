bc14b2f7c29016873ba09835cb1a51a10e2fc827
papousek11 <89077005+papousek11@users.noreply.github.com>
2026-01-31 19:34:04 +0100

    set up the project

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.VisualBasic;
+using Raylib_cs;
+
+
+
+namespace main;
+
+
+class Suguma
+{
+   
+
+    
+    //
+    static void Main(string[] args)
+    {
+       
+
+
+
+        //Vector2 tlacitko1_1 = new Vector2(10,350);
+      
+
+
+
+        Vector2 mouse_position;
+       
+
+
+       
+
+        
+
+
+        while (!Raylib.WindowShouldClose())
+        {
+
+            
+            mouse_position = Raylib.GetMousePosition();
+            //mezi = mouse_position.ToString();
+            Raylib.BeginDrawing();
+            Raylib.ClearBackground(Color.DarkBlue);
+           
+
+            
+            
+
+            Raylib.EndDrawing();
+            //Raylib.IsMouseButtonPressed(MouseButton.Left)
+           
+        }
+
+        Raylib.CloseWindow();
+
+
+    }
+
+  
+}
